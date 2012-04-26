8bb13e96e5abe9054eefcae021e1580867187cf0
mkleine <mkleine@coremedia.com>
2012-04-26 14:57:41 +0200

    use JSON object to parse JSON. Remember to put double-quotes around keys

--- server/js-nodejs/src/main/js/core-foos-sockets.js
+++ server/js-nodejs/src/main/js/core-foos-sockets.js
@@ -3,17 +3,16 @@ var socketIO = require('socket.io');
 var static = require('node-static');
 var repository = require('./core-foos-server');
 
-
 var config;
 try{
-  config = eval('(function(){return '+process.argv[2]+'})()');
-  console.log("config:");
-  console.dir(config);
+  config = JSON.parse(process.argv[2]);
 } catch(e){
   config = {dir:'./client'};
   console.warn("cannot parse config: "+ e);
   console.log("falling back to default config");
 }
+console.log("config:");
+console.dir(config);
 
 var clientFiles = new static.Server(config.dir);
 repository.initialize();
