97c5842f1c042df269f4db86b0de9e0087910ffc
Frauke Pantekoek <frauke.pantekoek@coremedia.com>
2012-04-27 11:50:48 +0200

    save request date, start date and end date for match, bugfix currentMatch

--- server/js-nodejs/src/main/js/core-foos-server.js
+++ server/js-nodejs/src/main/js/core-foos-server.js
@@ -33,7 +33,7 @@ var initialize = function (config) {
     users = new mongodb.Collection(client, usersCollection);
     users.ensureIndex({name:1}, {unique:true}, {});
     matches = new mongodb.Collection(client, matchesCollection);
-    // generateTestData(client);
+    //generateTestData(client);
     console.log("Really open");
   });
   console.log("ready for action");
@@ -42,33 +42,52 @@ var initialize = function (config) {
 var generateTestData = function (client) {
   requestPlay([
     {name:'Frauke'}
-  ]);
+  ], function () {
+  });
   requestPlay([
     {name:'Tom'}
-  ]);
+  ], function () {
+  });
   requestPlay([
     {name:'Moritz'}
-  ]);
+  ], function () {
+  });
   requestPlay([
     {name:'Kai'}
-  ]);
+  ], function () {
+  });
   requestPlay([
     {name:'xyz'}
-  ]);
-  requestMatch('user1', 'user2', 'user3', 'user4');
+  ], function () {
+    getListOfUsers(function (users) {
+      console.log("Num users: " + users.length)
+      cancelPlay('xyz');
+      getListOfUsers(function (users) {
+        console.log("Num users: " + users.length)
+      });
+    });
+  });
 
-  // cancelPlay('xyz');
+  requestMatch('user1', 'user2', 'user3', 'user4', function () {
+  });
+
   getListOfUsers(function (users) {
     console.log("Num users: " + users.length)
   });
 
-  matchPlayers();
+
   getNumberOfMatches(function (count) {
-    console.log("Count:", count);
+    console.log("Number of matches:", count);
   });
 
   startMatch(function () {
-
+    currentMatch(function (match) {
+      if (match) {
+        console.log("end match..." + match._id)
+        endMatch(match._id.toString(), function () {
+        });
+      }
+    })
   });
 };
 
@@ -118,7 +137,7 @@ var requestMatch = function (userName1, userName2, userName3, userName4, callbac
   mongo.upsert(users, {name:userName3}, {state:USER_STATE_MATCH_REQUESTED, date:new Date()}, function () {
   });
   mongo.upsert(users, {name:userName4}, {state:USER_STATE_MATCH_REQUESTED, date:new Date()}, function () {
-    mongo.insert(matches, {date:new Date(), player1:userName1, player2:userName2, player3:userName3, player4:userName4, state:MATCH_STATE_WAITING}, function () {
+    mongo.insert(matches, {requestDate:new Date(), player1:userName1, player2:userName2, player3:userName3, player4:userName4, state:MATCH_STATE_WAITING}, function () {
       startMatch(callback);
     });
   });
@@ -128,7 +147,6 @@ var requestMatch = function (userName1, userName2, userName3, userName4, callbac
 var matchPlayers = function (callback) {
   getListOfUsers(function (users) {
     if (users.length >= 4) {
-      console.log(users.length)
       requestMatch(users[0].name, users[1].name, users[2].name, users[3].name, callback);
     }
   });
@@ -141,22 +159,22 @@ var cancelPlay = function (userName) {
 
 var startMatch = function (callback) {
   mongo.find(matches, {state:MATCH_STATE_ACTIVE}, {}).toArray(function (err, result) {
-      if (result && result.length > 0) {
-        callback();
-      } else {
-        getListOfMatches(function (docs) {
-          if (docs && docs.length > 0) {
-            var match = docs[0];
-            var date = new Date();
-            mongo.upsert(matches, {_id:match._id}, {state:MATCH_STATE_ACTIVE, date: date}, function () {
-              console.log("Really start match");
-              callback({match: match, date: date});
-            });
-          } else {
-            callback();
-          }
-        });
-      }
+    if (result && result.length > 0) {
+      callback();
+    } else {
+      getListOfMatches(function (docs) {
+        if (docs && docs.length > 0) {
+          var match = docs[0];
+          var date = new Date();
+          mongo.upsert(matches, {_id:match._id}, {state:MATCH_STATE_ACTIVE, startDate:date}, function () {
+            console.log("Really start match");
+            callback({match:match, date:date});
+          });
+        } else {
+          callback();
+        }
+      });
+    }
   });
 };
 
@@ -172,7 +190,7 @@ var endMatch = function (id, callback) { //by id??
       });
       mongo.update(users, {name:match.userName4}, {state:USER_STATE_FINISHED}, function () {
       });
-      mongo.update(matches, {_id:match._id}, {state:MATCH_STATE_FINISHED}, function () {
+      mongo.update(matches, {_id:match._id}, {state:MATCH_STATE_FINISHED, endDate:new Date()}, function () {
         startMatch(callback);
       });
     }
@@ -183,9 +201,10 @@ var currentMatch = function (callback) {
   mongo.find(matches, {state:MATCH_STATE_ACTIVE}, {}).toArray(function (err, result) {
     if (result && result.length > 0) {
       callback(result[0]);
+      return;
     }
     callback();
-});
+  });
 };
 
 exports.getListOfUsers = getListOfUsers;
@@ -197,13 +216,13 @@ exports.initialize = initialize;
 exports.getNumberOfMatches = getNumberOfMatches;
 exports.getNumberOfActiveMatches = getNumberOfActiveMatches;
 exports.getListOfMatches = getListOfMatches;
-exports.administration = function(cmd,callback) {
-   if(cmd == "dropUsers"){
-     mongo.remove(users,{});
-   } else if(cmd == "dropMatches") {
-     mongo.remove(matches,{});
-   } else {
-     console.warn("unknown cmd: "+ cmd);
-   }
+exports.administration = function (cmd, callback) {
+  if (cmd == "dropUsers") {
+    mongo.remove(users, {});
+  } else if (cmd == "dropMatches") {
+    mongo.remove(matches, {});
+  } else {
+    console.warn("unknown cmd: " + cmd);
+  }
   callback();
 };
